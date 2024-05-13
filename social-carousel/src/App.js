c82d6497142210190ca95c3198058dfe1546acb5
Qasim <mq12113@gmail.com>
2024-05-13 17:06:43 +0100

    completed Carousel project

---
+++ social-carousel/src/App.js
@@ -0,0 +1,12 @@
+import "./App.css";
+import Carousel from "./Components/Carousel";
+
+function App() {
+  return (
+    <div className="App">
+      <Carousel url={"https://picsum.photos/v2/list"} page = {'1'} limit={"10"} />
+    </div>
+  );
+}
+
+export default App;
---
+++ social-carousel/src/Components/Carousel.js
@@ -0,0 +1,85 @@
+import { useState, useEffect } from "react";
+import { FaArrowLeft, FaArrowRight } from "react-icons/fa";
+import "../App.css";
+
+const Carousel = ({ url, limit = 5, page = 1 }) => {
+  const [images, setImages] = useState([]);
+  const [currentSlide, setCurrentSlide] = useState(0);
+  const [errorMsg, setErrorMsg] = useState(null);
+  const [loading, setLoading] = useState(false);
+
+  async function fetchImages(getUrl) {
+    try {
+      setLoading(true);
+      const response = await fetch(`${getUrl}?page=${page}&limit=${limit}`);
+      const data = await response.json();
+
+      if (data) {
+        setImages(data);
+        setLoading(false);
+      }
+    } catch (e) {
+      setErrorMsg(e.message);
+      setLoading(false);
+    }
+  }
+
+  function handlePrev() {
+    setCurrentSlide(currentSlide === 0 ? images.length - 1 : currentSlide - 1);
+  }
+  function handleNext() {
+    setCurrentSlide(currentSlide === images.length - 1 ? 0 : currentSlide + 1);
+  }
+
+  useEffect(() => {
+    if (url !== "") fetchImages(url);
+  }, [url, page, limit]);
+  console.log(images);
+
+  if (loading) {
+    return <div>Loading...</div>;
+  }
+
+  if (errorMsg !== null) {
+    return <div> Error occured! {errorMsg}</div>;
+  }
+  return (
+    <div className="container">
+      <FaArrowLeft onClick={handlePrev} className="arrow arrow-left" />
+      {images && images.length
+        ? images.map((imageItem, index) => (
+            <img
+              key={imageItem.id}
+              alt={imageItem.download_url}
+              src={imageItem.download_url}
+              className={
+                currentSlide === index
+                  ? "current-image"
+                  : "current-image hide-current-image"
+              }
+            />
+          ))
+        : null}
+
+      <FaArrowRight onClick={handleNext} className="arrow arrow-right" />
+
+      <span className="circle-indicator">
+        {images && images.length
+          ? images.map((_, index) => (
+              <button
+                key={index}
+                className={
+                  currentSlide === index
+                    ? "current-indicator"
+                    : "current-indicator inactive-indicator"
+                }
+                onClick={() => setCurrentSlide(index)}
+              ></button>
+            ))
+          : null}
+      </span>
+    </div>
+  );
+};
+
+export default Carousel;
