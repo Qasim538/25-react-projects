77e09ea1b9730eff542ab96676d6e6218140baf6
Qasim <mq12113@gmail.com>
2024-05-13 21:17:21 +0100

    completed loamore

---
+++ load-more/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import LoadMore from './Components/LoadMore';
+
+function App() {
+  return (
+    <div className="App">
+    <LoadMore />
+    
+    </div>
+  );
+}
+
+export default App;
