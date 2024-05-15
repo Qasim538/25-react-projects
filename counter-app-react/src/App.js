00f04c57495514e5ee79cb7cd4e7f90afa7f010b
Qasim <mq12113@gmail.com>
2024-05-15 16:27:25 +0100

    updated counter app

--- counter-app-react/src/App.js
+++ counter-app-react/src/App.js
@@ -8,18 +8,18 @@ function App() {
     <div className="App" my-5>
       <h1>Counter App</h1>
 
-      <h1 className="count"> {count} </h1>
+      <h1 style={{cursor: "pointer"}} className="count" onClick={() => setCount(0)} > {count} </h1>
 
       <div>
-        <button class="btn btn-success" onClick={() => setCount(count + 1)}>
-          Increment
-        </button>
-        <button disabled={count === 0} class="btn btn-secondary" onClick={() => setCount(count - 1)}>
-          Decrement
-        </button>
-        <button disabled={count === 0} class="btn btn-warning" onClick={() => setCount(0)}>
-          Reset
-        </button>
+      <button disabled={count === 0} class="btn btn-secondary" onClick={() => setCount(count - 1)}>
+      Decrement
+      </button>
+      <button disabled={count === 0} class="btn btn-warning" onClick={() => setCount(0)}>
+      Reset
+      </button>
+      <button class="btn btn-success" onClick={() => setCount(count + 1)}>
+        Increment
+      </button>
       </div>
     </div>
   );
