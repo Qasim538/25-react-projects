44488fc95be071d559e7254bb43c23384e83f621
Qasim <mq12113@gmail.com>
2024-05-16 22:41:00 +0100

    scroll progresss project complete

---
+++ 09-custome-scroll-indicator/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import ScrollIndicator from './Components/CustomeScroll';
+
+function App() {
+  return (
+    <div className="App">
+
+    <ScrollIndicator url={'https://dummyjson.com/products?200'}/>
+
+    </div>
+  );
+}
+
+export default App;
---
+++ 09-custome-scroll-indicator/src/Components/CustomeScroll.js
@@ -0,0 +1,75 @@
+import React, { useEffect, useState } from "react";
+
+const ScrollIndicator = ({ url }) => {
+  const [data, setData] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [errorMessage, setErrorMessage] = useState("");
+  const [scrollPercentage, setScrollPercentage] = useState(0);
+
+  async function fetchData(getUrl) {
+    try {
+      setLoading(true);
+      const response = await fetch(getUrl);
+      const data = await response.json();
+
+      if (data && data.products && data.products.length > 0) {
+        setData(data.products);
+        setLoading(false);
+      }
+    } catch (e) {
+      setErrorMessage(e.message);
+    }
+  }
+
+  useEffect(() => {
+    fetchData(url);
+  }, [url]);
+
+  function handleScrollPercentage() {
+    console.log(
+      document.body.scrollTop,
+      document.documentElement.scrollTop,
+      document.documentElement.scrollHeight,
+      document.documentElement.clientHeight
+    );
+    const howMuchScrolled =
+      document.body.scrollTop || document.documentElement.scrollTop;
+
+    const height =
+      document.documentElement.scrollHeight -
+      document.documentElement.clientHeight;
+
+    setScrollPercentage((howMuchScrolled / height) * 100);
+  }
+
+  useEffect(() => {
+    window.addEventListener("scroll", handleScrollPercentage);
+
+    return () => {
+      window.removeEventListener("scroll", () => {});
+    };
+  }, []);
+
+  console.log(data, scrollPercentage);
+
+  return (
+    <div>
+      <div className="top-container">
+        <h1>Custome Scroll Indicator</h1>
+        <div className="scroll-progress-track-cont">
+          <div
+            className="current-progress-bar"
+            style={{ width: `${scrollPercentage}%` }}
+          ></div>
+        </div>
+      </div>
+      <h2 className="data-container">
+        {data && data.length > 0
+          ? data.map((dataItem) => <p>{dataItem.title}</p>)
+          : null}
+      </h2>
+    </div>
+  );
+};
+
+export default ScrollIndicator;
