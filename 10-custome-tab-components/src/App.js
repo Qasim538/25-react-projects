bd462dd526542974c82ca345d6f57cc0bfe06650
Qasim <mq12113@gmail.com>
2024-05-17 22:32:46 +0100

    completed tabs

---
+++ 10-custome-tab-components/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import TabsTest from './Components/tabs-test'
+
+function App() {
+  return (
+    <div className="App">
+    <TabsTest />
+
+    </div>
+  );
+}
+
+export default App;
